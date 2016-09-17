bfe63068e50bd955fa73c595bee42ca1f243d34c
Kurt Kriese <kkriese@gmail.com>
2016-09-17 13:29:00 -0400

    final change for assignment

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,11 +1,12 @@
 (function () {
 'use strict';
 
-angular.module('LunchApp', [])
+angular.module('LunchCheck', [])
 .controller('LunchController', LunchController);
 
 LunchController.$inject = ['$scope'];
 function LunchController($scope) {
+  // allows string methods to be used
   if (!String.prototype.trim) {
      String.prototype.trim = function () {
      return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
@@ -14,13 +15,6 @@ function LunchController($scope) {
   $scope.countLunchItems = function() {
   var msgResponse = lunchDishMessage($scope.lunchDishes);
   $scope.responseMessage = msgResponse;
-  $scope.msgBorderColor = determineBorderColor(msgResponse);
-  if (msgResponse === "undefined") {
-    $scope.msgBorderStyle = ""
-  }
-  else {
-    $scope.msgBorderStyle = 'solid';
-  };
 };
 
   function lunchDishMessage(lunchItems) {
@@ -29,37 +23,25 @@ function LunchController($scope) {
       lunchItems=" ";
     };
     var trimmedLunchDishes = new String;
-    // if (!String.prototype.trim) {
-    //    String.prototype.trim = function () {
-    //    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
-    //     };
-    // };
     trimmedLunchDishes = String(lunchItems);
     trimmedLunchDishes = trimmedLunchDishes.trim();
     var msgString="";
     if (trimmedLunchDishes.length == 0 ) {
-    msgString= "Please enter data first";}
+      msgString= "Please enter data first";}
     else {
-      numberOfDishes = calcNumberOfDishes(trimmedLunchDishes);
-    if(numberOfDishes > 0 && numberOfDishes < 4) {
-         msgString="Enjoy!";}
-         else {
-             msgString="Too much!";}
-    };
+          numberOfDishes = calcNumberOfDishes(trimmedLunchDishes);
+          if(numberOfDishes > 0 && numberOfDishes < 4) {
+            msgString="Enjoy!";}
+          else {
+            msgString="Too much!";}
+        };
     return msgString;
   };
-    //
-  function calcNumberOfDishes(stringToSplit) {
-      var separator = ',';
-      var arrayOfDishes = stringToSplit.split(separator);
-      return arrayOfDishes.length
-    };
-  function determineBorderColor(strMessage){
-    var colorString='green';
-    if (strMessage=="Please enter data first"){
-      colorString = 'red';
-    };
-    return colorString;
+
+ function calcNumberOfDishes(stringToSplit) {
+     var separator = ',';
+     var arrayOfDishes = stringToSplit.split(separator);
+    return arrayOfDishes.length
   };
 };
 })();
