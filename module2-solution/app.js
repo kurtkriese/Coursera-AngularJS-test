6f410faa70c04fe9abe10cecd11541caffa3e1cd
Kurt Kriese <kkriese@gmail.com>
2016-09-26 22:10:48 -0400

    Final version of Module 2 Solution

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -12,28 +12,22 @@ function ShoppingListController($scope,ShoppingListServiceFactory) {
   $scope.SLCtrl = this;
   var SLCtrl = this;
   var shoppingListService = ShoppingListServiceFactory();
-  var buyItemQuantity = null;
-  var buyItemName = "";
-  var buyThisItem = Boolean(0===1);
+
   SLCtrl.isTBCtrlLoaded = Boolean(0===1);
-//  var blItems [];
+  SLCtrl.everthingBought = Boolean(0===1);
+  SLCtrl.nothingBought = Boolean(1===1);
   SLCtrl.bLItems = shoppingListService.getItems();
 
   SLCtrl.buyItem = function(thisItemName,thisItemQuantity) {
-//      var thisItem = TBCtrl.items[itemIndex]();
-     buyItemName = thisItemName;
-     buyItemQuantity = thisItemQuantity;
-     buyThisItem = Boolean(1===1);
+//     buyThisItem = Boolean(1===1);
      console.log("SLCtrl $scope: ", $scope);
-//    var test2 = SLCtrl.BLCtrl.buyItem(thisItemName,thisItemQuantity);
      shoppingListService.addItem(thisItemName,thisItemQuantity);
-//     TBCtrl.removeItem(itemIndex);
+     SLCtrl.nothingBought = Boolean(0===1);
   }
 };
 
-// LIST #1 - controller
+// LIST To Sell - controller
 ToBuyController.$inject = ['$scope','ShoppingListServiceFactory'];
-//ToBuyController.$inject = ['ShoppingListServiceFactory'];
 function ToBuyController($scope,ShoppingListServiceFactory) {
   var TBCtrl = this;
   $scope.TBCtrl=this;
@@ -46,9 +40,6 @@ function ToBuyController($scope,ShoppingListServiceFactory) {
     return Boolean(TBCtrl.tbitems.length > 0);
   };
 
-  //$scope.SLCtrl.isTBCtrlLoaded = TBCtrlLoaded();
-
-//  if (!isTBCtrlLoaded) {
 if (!$scope.SLCtrl.isTBCtrlLoaded) {
     toBuyShoppingList.addItem("cookies", 12 );
     toBuyShoppingList.addItem("bag of potato chips", 1);
@@ -59,7 +50,7 @@ if (!$scope.SLCtrl.isTBCtrlLoaded) {
     $scope.SLCtrl.isTBCtrlLoaded = TBCtrlLoaded();
  };
 
-  TBCtrl.emptyListMessage = Boolean(TBCtrl.tbitems.length < 1);
+  TBCtrl.emptyListMessage = $scope.SLCtrl.everthingBought;
   TBCtrl.itemName = "";
   TBCtrl.itemQuantity = "";
 
@@ -69,50 +60,29 @@ if (!$scope.SLCtrl.isTBCtrlLoaded) {
 
    TBCtrl.buyItem = function (itemIndex) {
     var thisItem = TBCtrl.tbitems[itemIndex];
-    var thisItemName = (thisItem.name);
+    var thisItemName = thisItem.name;
     var thisItemQuantity = thisItem.quantity;
     var thisIndexValue  = itemIndex;
-    // console.log("thisItem = ",thisItem );
-    // console.log("thisItemName = ",thisItemName );
-    // console.log("thisItemQuantity = ",thisItemQuantity );
-    // console.log("thisIndexValue = ",thisIndexValue );
-  //  $scope.SLCtrl.buyItem(TBCtrl.tbitems.name[itemIndex], TBCtrl.tbitems.quantity[itemIndex]);
     $scope.SLCtrl.buyItem(thisItemName,thisItemQuantity);
     toBuyShoppingList.removeItem(itemIndex);
+    $scope.SLCtrl.everthingBought = Boolean(TBCtrl.tbitems.length < 1);
  };
 };
 
-// LIST #2 - controller
- BLCtrlController.$inject = ['$scope','ShoppingListServiceFactory'];
- function BLCtrlController($scope,ShoppingListServiceFactory) {
-//BLCtrlController.$inject = ['ShoppingListServiceFactory'];
-//function BLCtrlController(ShoppingListServiceFactory) {
-  var BLCtrl = this;
+// Bought Items - controller
+ BLCtrlController.$inject = ['$scope'];
+ function BLCtrlController($scope) {
+   var BLCtrl = this;
    $scope.BLCtrl=this;
- var sLCtrl = $scope.SLCtrl;
-  // Use factory to create new shopping list service
-  var boughtShoppingList = ShoppingListServiceFactory();
+   var sLCtrl = $scope.SLCtrl;
 
-  //BLCtrl.items = boughtShoppingList.getItems();
   BLCtrl.items = sLCtrl.bLItems;
-  BLCtrl.emptyListMessage = function() {
-     Boolean(sLCtrl.items.length < 1)
-   };
-  BLCtrl.itemName = "";
   BLCtrl.itemQuantity = "";
-
-  // BLCtrl.buyItem = function (itemName, itemQuantity) {
-  //   if (sLCtrl.buyThisItem) {
-  //     boughtShoppingList.addItem(sLCtrl.buyItemName, sLCtrl.buyItemQuantity);
-  //   }
-  //}
 };
 
 // Service
 function ShoppingListService() {
   var service = this;
-
-  // List of shopping items
    var items = [];
 
   service.addItem = function (itemName, quantity) {
